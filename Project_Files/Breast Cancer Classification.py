323aae5be974588dd729788a80401560cf949617
Khaled <achechkhaled@gmail.com>
2020-08-16 12:24:24 +0200

    getting data and preparing the train models

---
+++ Project_Files/Breast Cancer Classification.py
@@ -0,0 +1,21 @@
+import sklearn
+from sklearn import datasets
+from sklearn import svm
+"""
+this program will help classify breast cancers into malignant or benign
+"""
+
+
+#here am going to import the breast cancer datasets
+cancer = datasets.load_breast_cancer()
+
+print(cancer.feature_names)
+print(cancer.target_names)
+
+x = cancer.data
+y = cancer.target
+
+x_train, x_test, y_train, x_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
+print(x_test,y_train)
+
+classes = ['malignant' 'benign']
